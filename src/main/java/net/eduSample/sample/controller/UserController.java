5408dd203f301efa3852a6fe3e89c90e03a9cbf4
jinyeonseok <jinyeonseok@gmail.com>
2020-10-16 22:26:52 +0900

    jenkins 배포 test

--- src/main/java/net/eduSample/sample/controller/UserController.java
+++ src/main/java/net/eduSample/sample/controller/UserController.java
@@ -172,7 +172,7 @@ public class UserController {
 		UserVO users = sampleService.userRead(identification);
 		model.addAttribute("UserVO", users);
 		log.info(users.toString());
-		// jenkins TEST
+		// jenkins TEST 1
 		
 	}	
 }
