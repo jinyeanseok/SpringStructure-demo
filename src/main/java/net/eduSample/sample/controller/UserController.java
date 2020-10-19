93c0e25e5c452de15e06de9dec1340c6f50ed028
jys <wdtcwek11@naver.com>
2020-10-19 13:17:56 +0900

    jenkins 배포 test

--- src/main/java/net/eduSample/sample/controller/UserController.java
+++ src/main/java/net/eduSample/sample/controller/UserController.java
@@ -172,6 +172,6 @@ public class UserController {
 		UserVO users = sampleService.userRead(identification);
 		model.addAttribute("UserVO", users);
 		log.info(users.toString());
-		// jenkins TEST 40
+		// jenkins TEST 41
 	}	
 }
