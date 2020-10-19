ba51955402ee7247e01d1a5c6677005d17f77e90
jys <wdtcwek11@naver.com>
2020-10-19 14:21:21 +0900

    jenkins DB 연동 TEST

--- src/main/java/net/eduSample/sample/controller/UserController.java
+++ src/main/java/net/eduSample/sample/controller/UserController.java
@@ -172,6 +172,6 @@ public class UserController {
 		UserVO users = sampleService.userRead(identification);
 		model.addAttribute("UserVO", users);
 		log.info(users.toString());
-		// jenkins TEST 44
-	}	
+		// jenkins TEST 45
+	}
 }
